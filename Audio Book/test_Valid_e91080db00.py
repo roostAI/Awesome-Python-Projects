4b5f26c7188164b9aeaad3a86c160c9d1c610acc
roostAI <108646238+roostAI@users.noreply.github.com>
2023-09-26 10:02:55 +0000

    Test generated by RoostGPT for test python-audio-book using AI Type Open AI and AI Model gpt-4

---
+++ Audio Book/test_Valid_e91080db00.py
@@ -0,0 +1,56 @@
+import unittest
+from sudoku import valid
+
+class TestValid(unittest.TestCase):
+
+    def test_Valid_e91080db00(self):
+        # Test case 1: Check if the number already exists in the row
+        board1 = [[5,3,0,0,7,0,0,0,0],
+                  [6,0,0,1,9,5,0,0,0],
+                  [0,9,8,0,0,0,0,6,0],
+                  [8,0,0,0,6,0,0,0,3],
+                  [4,0,0,8,0,3,0,0,1],
+                  [7,0,0,0,2,0,0,0,6],
+                  [0,6,0,0,0,0,2,8,0],
+                  [0,0,0,4,1,9,0,0,5],
+                  [0,0,0,0,8,0,0,7,9]]
+        self.assertEqual(valid(board1, 5, (0,2)), False)
+
+        # Test case 2: Check if the number already exists in the column
+        board2 = [[5,3,0,0,7,0,0,0,0],
+                  [6,0,0,1,9,5,0,0,0],
+                  [0,9,8,0,0,0,0,6,0],
+                  [8,0,0,0,6,0,0,0,3],
+                  [4,0,0,8,0,3,0,0,1],
+                  [7,0,0,0,2,0,0,0,6],
+                  [0,6,0,0,0,0,2,8,0],
+                  [0,0,0,4,1,9,0,0,5],
+                  [0,0,0,0,8,0,0,7,9]]
+        self.assertEqual(valid(board2, 6, (2,0)), False)
+
+        # Test case 3: Check if the number already exists in the box
+        board3 = [[5,3,0,0,7,0,0,0,0],
+                  [6,0,0,1,9,5,0,0,0],
+                  [0,9,8,0,0,0,0,6,0],
+                  [8,0,0,0,6,0,0,0,3],
+                  [4,0,0,8,0,3,0,0,1],
+                  [7,0,0,0,2,0,0,0,6],
+                  [0,6,0,0,0,0,2,8,0],
+                  [0,0,0,4,1,9,0,0,5],
+                  [0,0,0,0,8,0,0,7,9]]
+        self.assertEqual(valid(board3, 1, (1,2)), False)
+
+        # Test case 4: Check if the number does not exist in the row, column and box
+        board4 = [[5,3,0,0,7,0,0,0,0],
+                  [6,0,0,1,9,5,0,0,0],
+                  [0,9,8,0,0,0,0,6,0],
+                  [8,0,0,0,6,0,0,0,3],
+                  [4,0,0,8,0,3,0,0,1],
+                  [7,0,0,0,2,0,0,0,6],
+                  [0,6,0,0,0,0,2,8,0],
+                  [0,0,0,4,1,9,0,0,5],
+                  [0,0,0,0,8,0,0,7,9]]
+        self.assertEqual(valid(board4, 2, (0,2)), True)
+
+if __name__ == '__main__':
+    unittest.main()
